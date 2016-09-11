6552abdd84c2b82572f444bb77dc263d6358104b
Jeff Hood <jeffhood@gmail.com>
2016-09-11 12:50:34 -0500

    Moved methods to game_turn.rb

--- game.rb
+++ game.rb
@@ -21,15 +21,7 @@ class Game
     end
 
     @players.each do |player|
-      die = Die.new
-      case die.roll
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player.name} was skipped."
-      else
-        player.w00t
-      end
+      
       puts player
     end
 
